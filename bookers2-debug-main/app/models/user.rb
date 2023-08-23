3e223b8e4e457ec32b58a7c211ef83b4fababa31
YUU522 <kuromu2750@gmail.com>
2023-08-23 09:11:40 +0000

    [Finish]main3

--- bookers2-debug-main/app/models/user.rb
+++ bookers2-debug-main/app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction,length: { maximum: 50 }
   
   has_many :books
+  
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
