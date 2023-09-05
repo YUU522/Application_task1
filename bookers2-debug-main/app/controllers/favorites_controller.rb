2dcb7d26da2bdd3a0c04475815ee28c1c5bd6b89
YUU522 <kuromu2750@gmail.com>
2023-09-05 08:59:12 +0000

    [Application task6]hidouki

--- bookers2-debug-main/app/controllers/favorites_controller.rb
+++ bookers2-debug-main/app/controllers/favorites_controller.rb
@@ -1,15 +1,22 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
+    if favorite.save
+      respond_to do |format|
+      format.html { redirect_to @book }
+      format.js   # create.js.erb を実行
+      end
+    else
+      # エラーハンドリング
+    end
+    
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    
   end
 end
